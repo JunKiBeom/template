db4531a7d9253741e661e6ed5da4f70c968d1708
JunKiBeom <dona0408@gmail.com>
2025-01-24 05:06:05 +0900

    show write

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -2,12 +2,14 @@ package com.example.template.domain.post.post.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/posts")
 public class PostController {
 
-    @GetMapping("/")
+    @GetMapping("/write")
     @ResponseBody
     public String showWrite() {
         return """
